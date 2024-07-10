91e74fc896d93c00468d0aa92bf871f87f5e52cc
davidhcodes <dhanlyg1@gmail.com>
2024-07-10 16:36:28 +0100

    Changes the background colour of the user profile

--- fe-popcorner/app/(tabs)/UserInfo.jsx
+++ fe-popcorner/app/(tabs)/UserInfo.jsx
@@ -14,9 +14,11 @@ function UserInfo({ isLoggedIn, user, navigation, newUserInput, userInfo }) {
   useEffect(() => {
     if (userInfo?.interests && Object.keys(userInfo.interests).length > 0) {
       // Convert interests object to array
-      const newInterestsArray = Object.values(userInfo.interests).flatMap((category) => {
-        return Object.values(category);
-      });
+      const newInterestsArray = Object.values(userInfo.interests).flatMap(
+        (category) => {
+          return Object.values(category);
+        }
+      );
       setInterestsArray(newInterestsArray);
     } else {
       setInterestsArray([]);
@@ -25,95 +27,106 @@ function UserInfo({ isLoggedIn, user, navigation, newUserInput, userInfo }) {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      {/* <View style={styles.container}> */}
-      {userInfo ? (
-        <View style={styles.profileContainer}>
-          <Text style={styles.mainTitle}>
-            Hello {userInfo.firstName} {userInfo.lastName}
-          </Text>
+      <View style={styles.container}>
+        {userInfo ? (
+          <View className="w-full items-center justify-center bg-darkBackground ">
+            <Text style={styles.mainTitle}>
+              Hello {userInfo.firstName} {userInfo.lastName}
+            </Text>
 
-          {userInfo.avatar ? <Image source={{ uri: userInfo.avatar }} style={styles.avatar} /> : <Text>No avatar</Text>}
+            {userInfo.avatar ? (
+              <Image source={{ uri: userInfo.avatar }} style={styles.avatar} />
+            ) : (
+              <Text>No avatar</Text>
+            )}
 
-          <Text style={styles.title}> Communities: </Text>
-          {userInfo.communities && Object.keys(userInfo.communities).length > 0 ? (
-            Object.values(userInfo.communities).map((community, index) => (
-              <Text style={styles.interestBox} key={index}>
-                {community}
-              </Text>
-            ))
-          ) : (
-            <Text>Not a member of any communities</Text>
-          )}
-
-          <Text style={styles.title}> Cinemas: </Text>
-          {userInfo.cinemas && Object.keys(userInfo.cinemas).length > 0 ? (
-            Object.values(userInfo.cinemas).map((cinema, index) => (
-              <Text style={styles.interestBox} key={index}>
-                {cinema}
-              </Text>
-            ))
-          ) : (
-            <Text>No cinemas added</Text>
-          )}
-
-          <Text style={styles.title}> Events: </Text>
-          {userInfo.events && Object.keys(userInfo.events).length > 0 ? (
-            Object.values(userInfo.events).map((event, index) => (
-              <Text style={styles.interestBox} key={index}>
-                {event}
-              </Text>
-            ))
-          ) : (
-            <Text>No events added</Text>
-          )}
-
-          <Text style={styles.title}> Interests: </Text>
-          {interestsArray.length > 0 ? (
-            interestsArray.map(
-              (
-                specificInterest,
-                index // Ensure to specify the 'index' parameter in map function
-              ) => (
+            <Text style={styles.title}> Communities: </Text>
+            {userInfo.communities &&
+            Object.keys(userInfo.communities).length > 0 ? (
+              Object.values(userInfo.communities).map((community, index) => (
                 <Text style={styles.interestBox} key={index}>
-                  {/* Specify 'key' prop for each mapped element */}
-                  {specificInterest}
+                  {community}
                 </Text>
+              ))
+            ) : (
+              <Text>Not a member of any communities</Text>
+            )}
+
+            <Text style={styles.title}> Cinemas: </Text>
+            {userInfo.cinemas && Object.keys(userInfo.cinemas).length > 0 ? (
+              Object.values(userInfo.cinemas).map((cinema, index) => (
+                <Text style={styles.interestBox} key={index}>
+                  {cinema}
+                </Text>
+              ))
+            ) : (
+              <Text>No cinemas added</Text>
+            )}
+
+            <Text style={styles.title}> Events: </Text>
+            {userInfo.events && Object.keys(userInfo.events).length > 0 ? (
+              Object.values(userInfo.events).map((event, index) => (
+                <Text style={styles.interestBox} key={index}>
+                  {event}
+                </Text>
+              ))
+            ) : (
+              <Text>No events added</Text>
+            )}
+
+            <Text style={styles.title}> Interests: </Text>
+            {interestsArray.length > 0 ? (
+              interestsArray.map(
+                (
+                  specificInterest,
+                  index // Ensure to specify the 'index' parameter in map function
+                ) => (
+                  <Text style={styles.interestBox} key={index}>
+                    {/* Specify 'key' prop for each mapped element */}
+                    {specificInterest}
+                  </Text>
+                )
               )
-            )
-          ) : (
-            <Text>No interests added</Text>
-          )}
+            ) : (
+              <Text>No interests added</Text>
+            )}
 
-          <Text style={styles.title}> Moderator: </Text>
-          {userInfo.moderator && userInfo.moderator.moderatorOf && userInfo.moderator.moderatorOf.length > 0 ? (
-            userInfo.moderator.moderatorOf.map((moderatorOf, index) => (
-              <Text style={styles.interestBox} key={index}>
-                {moderatorOf}
-              </Text>
-            ))
-          ) : (
-            <Text>Not a moderator of any communities</Text>
-          )}
+            <Text style={styles.title}> Moderator: </Text>
+            {userInfo.moderator &&
+            userInfo.moderator.moderatorOf &&
+            userInfo.moderator.moderatorOf.length > 0 ? (
+              userInfo.moderator.moderatorOf.map((moderatorOf, index) => (
+                <Text style={styles.interestBox} key={index}>
+                  {moderatorOf}
+                </Text>
+              ))
+            ) : (
+              <Text>Not a moderator of any communities</Text>
+            )}
 
-          <Text style={styles.title}> Banned From: </Text>
-          {userInfo.isBannedFrom && Object.keys(userInfo.isBannedFrom).length > 0 ? (
-            Object.values(userInfo.isBannedFrom).map((bannedFrom, index) => (
-              <Text style={styles.interestBox} key={index}>
-                {bannedFrom}
-              </Text>
-            ))
-          ) : (
-            <Text>Not banned from any events</Text>
-          )}
+            <Text style={styles.title}> Banned From: </Text>
+            {userInfo.isBannedFrom &&
+            Object.keys(userInfo.isBannedFrom).length > 0 ? (
+              Object.values(userInfo.isBannedFrom).map((bannedFrom, index) => (
+                <Text style={styles.interestBox} key={index}>
+                  {bannedFrom}
+                </Text>
+              ))
+            ) : (
+              <Text>Not banned from any events</Text>
+            )}
 
-          <TouchableOpacity onPress={navigateToHome} style={styles.backButton}>
-            <Text style={styles.backButtonText}> Back to home </Text>
-          </TouchableOpacity>
-        </View>
-      ) : (
-        <Text>Waiting for user info</Text>
-      )}
-      {/* </View> */}
+            <TouchableOpacity
+              onPress={navigateToHome}
+              style={styles.backButton}
+            >
+              <Text style={styles.backButtonText}> Back to home </Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <Text>Waiting for user info</Text>
+        )}
+      </View>
     </ScrollView>
   );
 }
@@ -124,10 +137,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
+    // color: "#260101",
+    paddingTop: 30,
   },
   profileContainer: {
     width: "100%",
-    backgroundColor: "#fff",
+    // backgroundColor: "#fff",
+    color: "#260101",
     alignItems: "center",
     justifyContent: "center",
   },
--- fe-popcorner/tailwind.config.js
+++ fe-popcorner/tailwind.config.js
@@ -1,7 +1,16 @@
+const { default: colors } = require("./colors");
+
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#f57c00",
+      gray: "#C5C5C7",
+      mediumGray: "#F6F7FB",
+      lightGray: "#FAFAFA",
+      darkBackground: "#260101",
+    },
   },
   plugins: [],
 };
